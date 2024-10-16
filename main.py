7499b544626bfe639621e19dc155acbf662153fc
TaikiLazos <taikilazos@gmail.com>
2024-10-16 23:15:09 +0200

    with the current codebase you can get results for everything but SEGTHOR_AI and ALL

--- main.py
+++ main.py
@@ -67,6 +67,9 @@ datasets_params: dict[str, dict[str, Any]] = {}
 models = {'enet':ENet, 'unet':UNet, 'unetr':UNETR, 'shallowcnn':shallowCNN}
 datasets_params["TOY2"] = {'K': 2, 'net': shallowCNN, 'B': 2}
 datasets_params["SEGTHOR"] = {'K': 5, 'net': ENet, 'B': 8}  # Change net to ENet or UNet
+datasets_params["SEGTHOR_MED"] = {'K': 5, 'net': ENet, 'B': 8}
+datasets_params["SEGTHOR_AI"] = {'K': 5, 'net': ENet, 'B': 8} 
+datasets_params["SEGTHOR_ALL"] = {'K': 5, 'net': ENet, 'B': 8} 
 datasets_params["SEGTHOR_3D"] = {'K': 5, 'net': UNETR_monai, 'B': 4, 'img_shape': (128, 128, 64), 'input_dim': 1}
 
 
