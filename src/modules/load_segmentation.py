198ca9a7fa37145dbf7f57c2c78831bd603f9be3
JakobGroenenhaag <jakobgroenenhaag@gmail.com>
2024-10-20 17:58:13 +0200

    dice scores for post processing

--- src/modules/load_segmentation.py
+++ src/modules/load_segmentation.py
@@ -4,6 +4,7 @@ import numpy as np
 import glob
 from PIL import Image
 import torch
+import nibabel as nib
 
 _2d_pattern = re.compile(r"""Patient_(?P<patid>\d+)_(?P<layer>\d+)""", re.VERBOSE)
 
@@ -21,11 +22,27 @@ def parse_2d_filepath(path: Path):
     layer = int(match.group("layer"))
     return {"patient_id":patid, "layer":layer}
 
+def correct_k(array):
+    classes = set([0, 1, 2, 3, 4])
+    for k in np.unique(array):
+        if k not in classes:
+            return False
+    return True
+
 def to_onehot_tensor(array):
-    if not (np.unique(array) == np.array([0, 1, 2, 3, 4])).all():
+    if not correct_k(array):
         array = array // 63
-    assert (np.unique(array) == np.array([0, 1, 2, 3, 4])).all()
-    return torch.unsqueeze(torch.nn.functional.one_hot(torch.from_numpy(array).to(torch.int64)).T, dim=0)
+    assert correct_k(array)
+    t = torch.unsqueeze(torch.nn.functional.one_hot(torch.from_numpy(array).to(torch.int64)).T, dim=0)
+    if t.shape[1] < 5:
+        t = torch.nn.functional.pad(t, (0, 0, 0, 0, 0, 0, 0, 5 - t.shape[1]), value=0)
+    return t
+
+def save_as_nii(array, save_path):
+    if not correct_k(array):
+        array = array // 63
+    assert correct_k(array)
+    nib.save(nib.Nifti1Image(array, np.eye(4)), str(save_path))
 
 class Results:
     def __init__(self, path: Path):
@@ -44,7 +61,7 @@ class Results:
     def best_epoch_val_3d(self):
         assert self._best_epoch_val_patients is not None
         for patid in sorted(list(self._best_epoch_val_patients)):
-            yield to_onehot_tensor(stack_2d_imgs(self._root / "best_epoch" / "val", patid)), patid
+            yield stack_2d_imgs(self._root / "best_epoch" / "val", patid), patid
 
 
 class GT:
